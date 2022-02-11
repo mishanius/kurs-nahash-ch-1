90f36b828d622c262810b0d3b1006b8b6c85e317
t-mptitsyn <t-mptitsyn@microsoft.com>
2022-02-12 01:48:11 +0200

    removed results

--- nahash_1/excersise_1.py
+++ nahash_1/excersise_1.py
@@ -3,17 +3,9 @@ HELLO="asdad"
 
 # This is a function
 # TODO modify the hello_world so it would recive 2 paramets name and family name
-# TODO print the result 
-def hello_world(name, family):
+# TODO hello("ploni", "almoni") will return "hello ploni almoni"
+def hello_world(name):
     # this is variable assignment
-    to_print = "hello " + name + " " + family
+    to_print = "hello " + name
     # this is a return statemnt
     return to_print
-
-
-
-# we use the built-in print function to print what is returend from the hello_world function 
-print(hello_world("ENTER YOUR NAME HERE", "as"))
-
-# TODO modify the hello_world so it would recive 2 paramets name and family name
-# TODO print the result 
