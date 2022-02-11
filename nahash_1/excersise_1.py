1c3156f6fe38a14f1592e49d2a3824116c5b0b48
t-mptitsyn <t-mptitsyn@microsoft.com>
2022-02-12 01:13:13 +0200

    created nahash_1

---
+++ nahash_1/excersise_1.py
@@ -0,0 +1,19 @@
+HELLO="asdad"
+# This is a comment 123123
+
+# This is a function
+# TODO modify the hello_world so it would recive 2 paramets name and family name
+# TODO print the result 
+def hello_world(name, family):
+    # this is variable assignment
+    to_print = "hello " + name + " " + family
+    # this is a return statemnt
+    return to_print
+
+
+
+# we use the built-in print function to print what is returend from the hello_world function 
+print(hello_world("ENTER YOUR NAME HERE", "as"))
+
+# TODO modify the hello_world so it would recive 2 paramets name and family name
+# TODO print the result 
