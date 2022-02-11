b3f9fb7ff2d51e805880e49c3df7adbf12edae2c
t-mptitsyn <t-mptitsyn@microsoft.com>
2022-02-12 01:55:46 +0200

    solved ex1

--- nahash_1/excersise_1.py
+++ nahash_1/excersise_1.py
@@ -4,8 +4,8 @@ HELLO="asdad"
 # This is a function
 # TODO modify the hello_world so it would recive 2 paramets name and family name
 # TODO hello("ploni", "almoni") will return "hello ploni almoni"
-def hello_world(name):
+def hello_world(name, family):
     # this is variable assignment
-    to_print = "hello " + name
+    to_print = "hello " + name + "" + family
     # this is a return statemnt
     return to_print
