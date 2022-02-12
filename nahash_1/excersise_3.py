77340ef42606fdc61a97b98ad168db4b8941ec4f
Amit <amit.amihud@similarweb.com>
2022-02-12 11:38:09 +0200

    solved exercise 1

--- nahash_1/excersise_3.py
+++ nahash_1/excersise_3.py
@@ -2,7 +2,8 @@
 # example 1: basic_loop([1,2]) will return 3
 # example 2: basic_loop([1]) will return 1
 def basic_loop(list_of_numbers):
-    pass
+    return sum(list_of_numbers)  # this is the pythonic way
 
-if __name__=="__main__":
-    print(basic_loop([1,2,3]))
+
+if __name__ == "__main__":
+    print(basic_loop([1, 2, 3]))
