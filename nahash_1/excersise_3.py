7c4800f0e432c78dd10cb672461e4f63b34cddb7
michael-ptitsyn <ptitsyn@post.bgu.ac.il>
2022-02-26 11:33:39 +0200

    removed not needed files

--- nahash_1/excersise_3.py
+++
@@ -1,8 +0,0 @@
-# TODO In this excercise we are going to iterate over a list and sum all of its numbers
-# example 1: basic_loop([1,2]) will return 3
-# example 2: basic_loop([1]) will return 1
-def basic_loop(list_of_numbers):
-    pass
-
-if __name__=="__main__":
-    print(basic_loop([1,2,3]))
